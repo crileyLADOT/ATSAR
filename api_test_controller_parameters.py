49a5558fb0a1d9622e70aeae72dcf12910a59aa3
crileyLADOT <canyon.riley@lacity.org>
2020-02-11 10:24:38 -0800

    parameters show how to get api data by appending queries to the url

---
+++ api_test_controller_parameters.py
@@ -0,0 +1,23 @@
+import requests
+try:
+    import urlparse
+    from urllib import urlencode
+except: # For Python 3
+    import urllib.parse
+    from urllib.parse import urlencode
+
+url = "https://controllerdata.lacity.org/resource/pggv-e4fn.json"
+params = {'id_number':'6378807','department_name':'TRANSPORTATION','vendor_name':'VERIZON WIRELESS'}
+
+url_parts = list(urllib.parse.urlparse(url))                                                            #urlparse is a function that returns an obejct that acts like a tuple wtih 6 elements
+query = dict(urllib.parse.parse_qsl(url_parts[4]))                                                      #query is the 5th element in tuple
+query.update(params)
+
+url_parts[4] = urlencode(query)
+
+print(urllib.parse.urlunparse(url_parts))
+
+r = urllib.parse.urlunparse(url_parts)
+
+s = requests.get(r)
+print(s.text)
