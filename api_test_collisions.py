01dbe9e87f1f1101d91b0ef24950c8f1fc84fd40
crileyLADOT <canyon.riley@lacity.org>
2020-03-13 16:32:05 -0700

    Hard code url

--- api_test_collisions.py
+++ api_test_collisions.py
@@ -1,54 +1,77 @@
 import requests
+import json
 
-def twenty_10():
-    start_date = "2010-01-01T00:00:00.000"
-    end_date = "2010-12-31T00:00:00.000"
 
-def twenty_11():
-    start_date = "2011-01-01T00:00:00.000"
-    end_date = "2011-12-31T00:00:00.000"
 
-def twenty_12():
-    start_date = "2012-01-01T00:00:00.000"
-    end_date = "2012-12-31T00:00:00.000"
-
-def twenty_13():
-    start_date = "2013-01-01T00:00:00.000"
-    end_date = "2013-12-31T00:00:00.000"
-
-def twenty_14():
-    start_date = "2014-01-01T00:00:00.000"
-    end_date = "2014-12-31T00:00:00.000"
-
-def twenty_15():
-    start_date = "2015-01-01T00:00:00.000"
-    end_date = "2015-12-31T00:00:00.000"
-
-def twenty_16():
-    start_date = "2016-01-01T00:00:00.000"
-    end_date = "2016-12-31T00:00:00.000"
-
-def twenty_17():
-    start_date = "2017-01-01T00:00:00.000"
-    end_date = "2017-12-31T00:00:00.000"
-
-def twenty_18():
-    start_date = "2018-01-01T00:00:00.000"
-    end_date = "2018-12-31T00:00:00.000"
-
-def twenty_19():
-    start_date = "2019-01-01T00:00:00.000"
-    end_date = "2019-12-31T00:00:00.000"
 
 ################################################################################
+
+
+print("Total number of yearly collisions data \n")
+print("\n")
+print("\n")
 
 i = True
 
 while i:
-    choice = str(input("Choose a year: \n"))
-    if choice == '2010':
-        twenty_10()
-        helloworld
+    choice = str(input("Choose a year between 2010 and 2019: \n"))
+    if choice == "2010":
+        url = "https://data.lacity.org/resource/d5tf-ez2w.json?$where=date_occ+between+'2010-01-01T00:00:00.000'+and+'2010-12-31T00:00:00.000'"
+        break
+
+    elif choice == "2011":
+        url = "https://data.lacity.org/resource/d5tf-ez2w.json?$where=date_occ+between+'2011-01-01T00:00:00.000'+and+'2011-12-31T00:00:00.000'"
+        break
+
+    elif choice == "2012":
+        url = "https://data.lacity.org/resource/d5tf-ez2w.json?$where=date_occ+between+'2012-01-01T00:00:00.000'+and+'2012-12-31T00:00:00.000'"
+        break
+
+    elif choice == "2013":
+        url = "https://data.lacity.org/resource/d5tf-ez2w.json?$where=date_occ+between+'2013-01-01T00:00:00.000'+and+'2013-12-31T00:00:00.000'"
+        break
+
+    elif choice == "2014":
+        url = "https://data.lacity.org/resource/d5tf-ez2w.json?$where=date_occ+between+'2014-01-01T00:00:00.000'+and+'2014-12-31T00:00:00.000'"
+        break
+
+    elif choice == "2015":
+        url = "https://data.lacity.org/resource/d5tf-ez2w.json?$where=date_occ+between+'2015-01-01T00:00:00.000'+and+'2015-12-31T00:00:00.000'"
+        break
+
+    elif choice == "2016":
+        url = "https://data.lacity.org/resource/d5tf-ez2w.json?$where=date_occ+between+'2016-01-01T00:00:00.000'+and+'2016-12-31T00:00:00.000'"
+        break
+
+    elif choice == "2017":
+        url = "https://data.lacity.org/resource/d5tf-ez2w.json?$where=date_occ+between+'2017-01-01T00:00:00.000'+and+'2017-12-31T00:00:00.000'"
+        break
+
+    elif choice == "2018":
+        url = "https://data.lacity.org/resource/d5tf-ez2w.json?$where=date_occ+between+'2018-01-01T00:00:00.000'+and+'2018-12-31T00:00:00.000'"
+        break
+
+    elif choice == "2019":
+        url = "https://data.lacity.org/resource/d5tf-ez2w.json?$where=date_occ+between+'2019-01-01T00:00:00.000'+and+'2019-12-31T00:00:00.000'"
+        break
+
+    else:
+        print("Invalid date try again")
+        break
+
+
+s = requests.get(url)
+
+json_object = json.loads(s.text)
+
+json_pretty = json.dumps(json_object, indent=2)
+
+print(json_pretty)
+
+
+
+
+
 
 
 #url = "https://data.lacity.org/resource/d5tf-ez2w.json?$where=date_occ+between+'2015-01-10T00:00:00.000'+and+'2015-01-11T00:00:00.000'"
